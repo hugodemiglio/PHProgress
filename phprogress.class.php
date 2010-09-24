815d1894f20a8dd261e5c4ed13158a1594389ab5
Hugo <hugo@formaweb.com.br>
2010-09-24 20:23:19 -0300

    Exemplo criado e funcionando

--- phprogress.class.php
+++ phprogress.class.php
@@ -190,18 +190,5 @@ class Porcentagem{
 	}
 	
 }
-/*
-$tela = new Porcentagem;
-$tela->inicio();
-$tela->turbo(true);
-$tela->mostrar(array('barra'=>true,'tempo'=>true,'roda'=>true,'fps'=>true));
-
-for($i = 1;$i <= 100; $i = $i + 0.001){
-	$tela->exibir($i);
-}
-
-$tela->concluido();*/
-
-echo get_env();
 
 ?>
